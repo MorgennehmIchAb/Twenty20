2090e9222f5ac308ade554b794c2f36e56f1122f
Daniel Peil <daniel.peil@outlook.com>
2020-08-03 12:28:22 +0200

    TestNachricht

---
+++ Twenty20/Controllers/GitHookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Twenty20.Controllers
+{
+    [Route("[Controller]")]
+    public class GitHookController : Controller
+    {
+        public async Task<IActionResult> Receive()
+        {
+            return Ok();
+        }
+    }
+}
